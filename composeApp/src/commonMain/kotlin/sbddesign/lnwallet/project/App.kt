0fda825a8660667f4b0ad23e6e3e2ab24216df12
sbddesign <stephen@d.elor.me>
2025-10-31 15:27:30 -0400

    update text test

--- composeApp/src/commonMain/kotlin/sbddesign/lnwallet/project/App.kt
+++ composeApp/src/commonMain/kotlin/sbddesign/lnwallet/project/App.kt
@@ -32,7 +32,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+                Text("Bitcoin Rocks!")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
