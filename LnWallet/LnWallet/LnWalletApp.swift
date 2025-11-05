6eaa66d62a1ef3a7046a07dee0282eea629da33d
sbddesign <stephen@d.elor.me>
2025-11-05 16:12:25 -0500

    setup xcode project

---
+++ LnWallet/LnWallet/LnWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  LnWalletApp.swift
+//  LnWallet
+//
+//  Created by Stephen DeLorme on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LnWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
